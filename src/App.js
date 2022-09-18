93d70a0eb67fa2cf7cb3f74ac4897358bc83c4d6
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2022-09-18 14:25:18 -0500

    add complete link and delete unused css

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
       <p>We have a bin of robot parts in a factory. Each part goes to a robot with a specific, unique name. Each part will be described by a string, with the name of the robot and the part name separated by an underscore, like "Rocket_arm".
         
       All robots are made of the same types of parts, and we have a string of all of the parts required to form a complete robot. Given a list of available parts, return the collection of robot names for which we can build at least one complete robot.</p>
+
+      <p>See the full solution <a href='https://github.com/bkleeman/part-matcher/blob/master/src/Components/PartMatcher.js' target="_blank" rel="noreferrer">here</a>!</p>
       <PartMatcher/>
     </div>
   );
